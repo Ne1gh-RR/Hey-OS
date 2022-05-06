83c7dcb1fdda2b5cea63712cb9868da0e2add9f4
Ne1gh_ <101720082+Ne1gh-RR@users.noreply.github.com>
2022-05-06 11:00:25 +0500

    Add files via upload

---
+++ BIOS/BIOS/CategorySelector.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIOS
+{
+    public partial class MainWindow
+    {
+        public void SelectCategoryFromRightArrow(ref SelectedCategory category)
+        {
+            switch (category)
+            {
+                case SelectedCategory.Main:
+                    SelectAdvanced();
+                    CancelSelectingMain();
+                    break;
+                case SelectedCategory.Advanced:
+                    SelectSecurity();
+                    CancelSelectingAdvanced();
+                    break;
+                case SelectedCategory.Security:
+                    SelectBoot();
+                    CancelSelectingSecurity();
+                    break;
+                case SelectedCategory.Boot:
+                    SelectExit();
+                    CancelSelectingBoot();
+                    break;
+                case SelectedCategory.Exit:
+                    SelectMain();
+                    CancelSelectingExit();
+                    break;
+            }
+        }
+        public void SelectCategoryFromLeftArrow(ref SelectedCategory category)
+        {
+            switch (category)
+            {
+                case SelectedCategory.Exit:
+                    SelectBoot();
+                    CancelSelectingExit();
+                    break;
+                case SelectedCategory.Boot:
+                    SelectSecurity();
+                    CancelSelectingBoot();
+                    break;
+                case SelectedCategory.Security:
+                    SelectAdvanced();
+                    CancelSelectingSecurity();
+                    break;
+                case SelectedCategory.Advanced:
+                    SelectMain();
+                    CancelSelectingAdvanced();
+                    break;
+                case SelectedCategory.Main:
+                    SelectExit();
+                    CancelSelectingMain();
+                    break;
+            }
+        }
+    }
+}
